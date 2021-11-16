3b6ad89ca140ecd1d9cd80f06e58397a370cb8a8
pebpung <mcuwwa29@gmail.com>
2021-11-17 00:55:59 +0900

    add IoU

--- eval.py
+++ eval.py
@@ -12,6 +12,7 @@ from efficientunet import *
 from dataset import *
 from utils import *
 from config import *
+from metric import iou_score
 
 # Evaluation
 # 1. Delete RandomFlip
@@ -31,10 +32,6 @@ transform = A.Compose([
     A.Normalize(mean=(0.485), std=(0.229)),
     transforms.ToTensorV2(),
     ])
-
-RESULTS_DIR = os.path.join(ROOT_DIR, 'test_results')
-if not os.path.exists(RESULTS_DIR):
-    os.makedirs(RESULTS_DIR)
 
 test_dataset = Dataset(imgs_dir=TEST_IMGS_DIR, mask_dir=TEST_LABELS_DIR, transform=transform)
 test_loader = DataLoader(test_dataset, batch_size=cfg.BATCH_SIZE, shuffle=False, num_workers=0)
@@ -60,7 +57,7 @@ if os.listdir(CKPT_DIR):
 # Evaluation
 with torch.no_grad():
     net.eval()  # Evaluation Mode
-    loss_arr = list()
+    loss_arr, iou_arr = [], []
 
     for batch_idx, data in enumerate(test_loader, 1):
         # Forward Propagation
@@ -70,18 +67,22 @@ with torch.no_grad():
         label = label // 255
 
         output = net(img)
+        output_t = torch.argmax(output, dim=1).float()
 
         # Calc Loss Function
         loss = loss_fn(output, label)
+        iou = iou_score(output_t, label)
+
         loss_arr.append(loss.item())
+        iou_arr.append(iou.item())
         
-        print_form = '[Test] | Batch: {:0>4d} / {:0>4d} | Loss: {:.4f}'
-        print(print_form.format(batch_idx, test_batch_num, loss_arr[-1]))
+        print_form = '[Test] | Batch: {:0>4d} / {:0>4d} | Loss: {:.4f} | IoU: {:.4f}'
+        print(print_form.format(batch_idx, test_batch_num, loss_arr[-1], iou))
 
-        output_t = torch.argmax(output, dim=1).float()
-
-        # Tensorboard
         img = to_numpy(denormalization(img, mean=0.5, std=0.5))
+        # 이미지 캐스팅
+        img = np.clip(img, 0, 1) 
+
         label = to_numpy(label)
         output_t = to_numpy(classify_class(output_t))
         
@@ -92,5 +93,5 @@ with torch.no_grad():
             plt.imsave(os.path.join(RESULTS_DIR, f'label_{crt_id:04}.png'), label[j].squeeze(), cmap='gray')
             plt.imsave(os.path.join(RESULTS_DIR, f'output_{crt_id:04}.png'), output_t[j].squeeze(), cmap='gray')
             
-print_form = '[Result] | Avg Loss: {:0.4f}'
-print(print_form.format(np.mean(loss_arr)))
+print_form = '[Result] | Avg Loss: {:0.4f} | Avg IoU: {:0.4f}'
+print(print_form.format(np.mean(loss_arr), np.mean(iou_arr)))
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from efficientunet import *
 from dataset import *
 from config import *
 from utils import *
+from metric import iou_score
 
 cfg = Config()
 
@@ -35,8 +36,6 @@ val_transform = A.Compose([
     transforms.ToTensorV2(),
 ])
 
-train_dataset =  Dataset(imgs_dir=TRAIN_IMGS_DIR, mask_dir=TRAIN_LABELS_DIR, transform=train_transform)
-
 # Set Dataset
 train_dataset = Dataset(imgs_dir=TRAIN_IMGS_DIR, mask_dir=TRAIN_LABELS_DIR, transform=train_transform)
 train_loader = DataLoader(train_dataset, batch_size=cfg.BATCH_SIZE, shuffle=True, num_workers=0)
@@ -51,7 +50,7 @@ val_batch_num = int(np.ceil(val_data_num / cfg.BATCH_SIZE))
 
 # Network
 # net = UNet().to(device)
-net = get_efficientunet_b0(out_channels=2, concat_input=True, pretrained=True).to(device)
+net = get_socar_efficientunet_b0(out_channels=2, concat_input=True, pretrained=True).to(device)
 
 # Loss Function
 loss_fn = nn.CrossEntropyLoss().to(device)
@@ -67,7 +66,7 @@ num_epochs = cfg.NUM_EPOCHS
 
 for epoch in range(start_epoch+1, num_epochs+1):
     net.train()  # Train Mode
-    train_loss_arr = list()
+    train_loss_arr = []
     
     for batch_idx, data in enumerate(train_loader, 1):
         # Forward Propagation
@@ -77,17 +76,19 @@ for epoch in range(start_epoch+1, num_epochs+1):
         label = label // 255
         
         output = net(img)
-        
+
         # Backward Propagation
         optim.zero_grad()
         
         loss = loss_fn(output, label)
+
         loss.backward()
         
         optim.step()
         
         # Calc Loss Function
         train_loss_arr.append(loss.item())
+
         print_form = '[Train] | Epoch: {:0>4d} / {:0>4d} | Batch: {:0>4d} / {:0>4d} | Loss: {:.4f}'
         print(print_form.format(epoch, num_epochs, batch_idx, train_batch_num, train_loss_arr[-1]))
 
@@ -96,7 +97,7 @@ for epoch in range(start_epoch+1, num_epochs+1):
     # Validation (No Back Propagation)
     with torch.no_grad():
         net.eval()  # Evaluation Mode
-        val_loss_arr = list()
+        val_loss_arr, val_iou_arr = [], []
         
         for batch_idx, data in enumerate(val_loader, 1):
             # Forward Propagation
@@ -106,19 +107,24 @@ for epoch in range(start_epoch+1, num_epochs+1):
             label = label // 255
 
             output = net(img)
+            output_t = torch.argmax(output, dim=1).float()
             
             # Calc Loss Function
             loss = loss_fn(output, label)
+            iou = iou_score(output_t, label)
+
             val_loss_arr.append(loss.item())
+            val_iou_arr.append(iou.item())
             
-            print_form = '[Validation] | Epoch: {:0>4d} / {:0>4d} | Batch: {:0>4d} / {:0>4d} | Loss: {:.4f}'
-            print(print_form.format(epoch, num_epochs, batch_idx, val_batch_num, val_loss_arr[-1]))
+            print_form = '[Validation] | Epoch: {:0>4d} / {:0>4d} | Batch: {:0>4d} / {:0>4d} | Loss: {:.4f} | IoU: {:.4f}'
+            print(print_form.format(epoch, num_epochs, batch_idx, val_batch_num, val_loss_arr[-1], iou))
             
     val_loss_avg = np.mean(val_loss_arr)
+    val_iou_avg =  np.mean(val_iou_arr)
     # val_writer.add_scalar(tag='loss', scalar_value=val_loss_avg, global_step=epoch)
     
-    print_form = '[Epoch {:0>4d}] Training Avg Loss: {:.4f} | Validation Avg Loss: {:.4f}'
-    print(print_form.format(epoch, train_loss_avg, val_loss_avg))
+    print_form = '[Epoch {:0>4d}] Training Avg Loss: {:.4f} | Validation Avg Loss: {:.4f} | Validation Avg IoU: {:.4f}'
+    print(print_form.format(epoch, train_loss_avg, val_loss_avg, val_iou_avg))
     
     save_net(ckpt_dir=CKPT_DIR, net=net, optim=optim, epoch=epoch)
     
