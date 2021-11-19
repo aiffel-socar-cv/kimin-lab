52ee7116594556a0b2a636aa1441e604c066ef48
pebpung <mcuwwa29@gmail.com>
2021-11-19 15:34:22 +0900

    add sweep

--- config.py
+++ config.py
@@ -1,17 +1,42 @@
 import os
 import torch
 
+
+sweep_config = {
+    'method': 'grid',
+    'name':'grid-socar_sweep_v1',
+    'metric' : {
+        'name': 'val_epoch_iou',
+        'goal': 'maximize'   
+        },
+    'parameters' : {
+        'epochs': {
+            'value' : 50},
+        'batch_size': {
+            'value' : 15},
+        'optimizer': { 
+            'value': 'adabelief'}, # ['adam', 'adabelief']},
+        'model': { 
+             'values': ['ssl-b0','ssl-b1']}, # 'values': ['ssl-b1', 'imagenet-b1', 'ssl-b0', 'imagenet-b0']},
+        'loss': { 
+             'values': ['focal', 'cross_entropy']}, # 'values': ['ssl-b1', 'imagenet-b1', 'ssl-b0', 'imagenet-b0']},
+        'seed':{
+            'value': 0},
+        'learning_rate': {
+            'value': 1e-3}, #'values': [1e-3, 1e-4]},
+    }
+}
+
+
 def mkdir(*paths):
     for path in paths:
         if not os.path.exists(path):
             os.makedirs(path, exist_ok=True)
 
-name = 'dent_mask_only_ssl'
-
 ROOT_DIR = os.path.join('/home/pung/repo/', 'kimin-lab')
 DATA_DIR = os.path.join(ROOT_DIR, 'accida_segmentation/dent_mask_only' )
-CKPT_DIR = os.path.join(ROOT_DIR, 'checkpoints_dir', 'checkpoints_' + name)
-RESULTS_DIR = os.path.join(ROOT_DIR, 'test_results_dir', 'test_results_' + name)
+CKPT_DIR = os.path.join(ROOT_DIR, 'checkpoints_dir', 'checkpoints_')
+RESULTS_DIR = os.path.join(ROOT_DIR, 'test_results_dir', 'test_results_')
 
 IMGS_DIR = os.path.join(DATA_DIR, 'imgs')
 LABELS_DIR = os.path.join(DATA_DIR, 'labels')
@@ -30,6 +55,7 @@ mkdir(
     )
 
 device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+
 
 # Hyper parameters
 class Config:
